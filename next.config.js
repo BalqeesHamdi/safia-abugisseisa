c1e6e97e7c3932ce75717e245475b3749f798ef2
BalqeesHamdi <Balqees.icode@gmail.com>
2024-06-24 13:14:35 +0300

    config changes

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ const nextConfig = {
   images:{
     unoptimized:true
   },
-  basePath: '/safia-abugisseisa',
+  basePath: '/safia-abugisseisa/',
   // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
   // trailingSlash: true,
  
