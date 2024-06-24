4faa9fbfedfbe3bd2d2c688d5b05634fa4d70aa3
BalqeesHamdi <Balqees.icode@gmail.com>
2024-06-24 13:11:27 +0300

    deployment changes

--- next.config.js
+++ next.config.js
@@ -1,8 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  basePath: "/safia-abugisseisa",
-  output: "export",
-  reactStrictMode: true,
-};
 
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const isProd = process.env.NODE_ENV === 'production';
+
+const nextConfig = {
+  images:{
+    unoptimized:true
+  },
+  basePath: '/safia-abugisseisa',
+  // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
+  // trailingSlash: true,
+ 
+  // Optional: Prevent automatic `/me` -> `/me/`, instead preserve `href`
+  // skipTrailingSlashRedirect: true,
+ 
+  // Optional: Change the output directory `out` -> `dist`
+};
+ 
 module.exports = nextConfig;
