3702f7372fd2bf7eec774789a7b8fc4705c1cd56
BalqeesHamdi <Balqees.icode@gmail.com>
2024-07-23 13:13:55 +0300

    eslint fixes

--- pages/Home_ar.js
+++ pages/Home_ar.js
@@ -37,47 +37,47 @@ const HomeAr = () => {
         </div>
       </div>
       <a name="intro">
-      <div className="content">
-        <p>صفية أبوقصيصة هو مكتب قانوني يضع التفكير المستقبلي في صميم ممارساته القانونية المبتكرة والمخصصة لتمكين الشركات من الحلول القانونية المبتكرة.
+        <div className="content">
+          <p>صفية أبوقصيصة هو مكتب قانوني يضع التفكير المستقبلي في صميم ممارساته القانونية المبتكرة والمخصصة لتمكين الشركات من الحلول القانونية المبتكرة.
           نحن في صفية أبو قصيصة ، ندرك أهمية البقاء في طليعة المشهد التجاري العالمي المترابط في الوقت الحاضر.
           التزامنا بالاستفادة من التكنولوجيا والابتكار يتيح لنا تقديم حلول قانونية مخصصة تلبي الاحتياجات المتطورة لعملائنا. مع التركيز على السرعة والكفاءة، نسعى لتقديم نصائح سلسة ترتكز على المعرفة العميقة والخبرة المهنية.
           نهجنا المعاصر، بالاشتراك مع التزامنا بالتميز، يميزنا كشريك موثوق به للشركات التي تبحث عن دعم قانوني استراتيجي.</p>
-      </div>
+        </div>
       </a>
       <a name="mission">
-      <div className="image-block reversed">
-        <div className="image"></div>
-        <div className="image-block-text" style={{ marginRight: '-150px' }}>
-          <h1>مهمتنا</h1>
-          <p>مهمتنا هي دعم الشركات الناشئة والشركات الصغيرة والمتوسطة، وتعزيز الابتكار والنمو والفرص باستمرار من خلال تقديم الحلول القانونية المبتكرة التي تتجاوب مع احتياجات السرعة في منظومة الأعمال الحالية.
+        <div className="image-block reversed">
+          <div className="image"></div>
+          <div className="image-block-text" style={{ marginRight: '-150px' }}>
+            <h1>مهمتنا</h1>
+            <p>مهمتنا هي دعم الشركات الناشئة والشركات الصغيرة والمتوسطة، وتعزيز الابتكار والنمو والفرص باستمرار من خلال تقديم الحلول القانونية المبتكرة التي تتجاوب مع احتياجات السرعة في منظومة الأعمال الحالية.
             بالتركيز على دعم الابتكار وريادة الأعمال، نهدف إلى تقديم حلول قانونية مخصصة تتناسب مع التحديات المحددة التي تواجه هذه الفئات الديناميكية.
             من خلال إدخال منظور جديد ودماء جديدة في نهجنا، نسعى إلى إلهام ودعم الجيل القادم من قادة الأعمال في تحقيق أهدافهم بثقة وخبرة قانونية.</p>
+          </div>
         </div>
-      </div>
       </a>
       <a name="vision">
-      <div className="content full dubai-artline" style={{ backgroundPosition: 'bottom left' }} dir="rtl">
-        <h1>رؤيتنا</h1>
-        <p style={{ textAlign: 'right' }}>أن نكون الوجهة للحلول القانونية المبتكرة المصممة خصيصًا لمنظومة الأعمال الديناميكية في دبي والشرق الأوسط وشمال أفريقيا. نطمح في أن نكون المنصة المفضلة للشركات الناشئة والشركات الصغيرة والمتوسطة التي تبحث عن خبرة قانونية متطورة تدعم النمو وتعزز الابتكار وتفتح الفرص في هذه المنطقة النابضة بالحياة.
+        <div className="content full dubai-artline" style={{ backgroundPosition: 'bottom left' }} dir="rtl">
+          <h1>رؤيتنا</h1>
+          <p style={{ textAlign: 'right' }}>أن نكون الوجهة للحلول القانونية المبتكرة المصممة خصيصًا لمنظومة الأعمال الديناميكية في دبي والشرق الأوسط وشمال أفريقيا. نطمح في أن نكون المنصة المفضلة للشركات الناشئة والشركات الصغيرة والمتوسطة التي تبحث عن خبرة قانونية متطورة تدعم النمو وتعزز الابتكار وتفتح الفرص في هذه المنطقة النابضة بالحياة.
           أن نكون معروفين في منطقة الشرق الأوسط وشمال أفريقيا بنهجنا المتطور، واستغلال التكنولوجيا والابتكار مع الخبرة والمعرفة القانونية لتقديم حلول قانونية متطورة. نهدف إلى تجاوز توقعات العملاء بشكل مستمر من خلال تقديم خدمات قانونية عالية الجودة ومتخصصة تدعم النجاح وتضيف قيمة للشركات في جميع أنحاء العالم.</p>
-      </div>
+        </div>
       </a>
       <a name="experties">
-      <div className="colored-background experties" dir="rtl">
-        <h1>خبراتنا</h1>
-        <div className="boxes-container">
-          {experties.map((item, index) => {
-            if (index < 6)
-              return (
-                <div className="box" key={item}>{item}</div>
-              );
-            else if (show && index > 5) {
-              return (
-                <div className="box" style={!show ? { display: 'none' } : {}} key={item} >{item}</div>
-              );
-            }
-          })}
-          {
+        <div className="colored-background experties" dir="rtl">
+          <h1>خبراتنا</h1>
+          <div className="boxes-container">
+            {experties.map((item, index) => {
+              if (index < 6)
+                return (
+                  <div className="box" key={item}>{item}</div>
+                );
+              else if (show && index > 5) {
+                return (
+                  <div className="box" style={!show ? { display: 'none' } : {}} key={item} >{item}</div>
+                );
+              }
+            })}
+            {
             show ?
               <div className="show-button" onClick={() => setShow(!show)} >
                 <p>عرض نتائج اقل<i className="fas fa-fw fa-chevron-up" style={{marginRight:'4px'}} /></p>
@@ -85,62 +85,62 @@ const HomeAr = () => {
               : <div className="show-button" onClick={() => setShow(!show)} >
                 <p>عرض المزيد<i className="fas fa-fw fa-chevron-down" style={{marginRight:'4px'}} /> </p>
               </div>
-          }
+            }
+          </div>
         </div>
-      </div>
       </a>
       <a name="team">
-      <div style={{paddingTop:'30px'}}>
-        <div style={{ backgroundImage: `url("${process.env.BASE_URL}/Rectangle 40.png")`, backgroundSize: 'contain', height: '100vh' }}>
-          <h1 style={{textAlign:'center',paddingBottom:'20px'}}>فريقنا</h1>
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
-          <div  style={{width:'40%', marginRight:'20px',display:'flex',paddingLeft:'20%',paddingTop:'0', paddingBottom:'0'}}>
-              <Image src={`${process.env.BASE_URL}/team_safia.png`} alt='' width={100} height={100} style={{height:'100%', width:'100%'}} />
-              <div>
-              <h1 style={{margin:'20px 0',fontSize:'70px',marginLeft:'-80px'}}>صفية أبوقصيصة</h1>
+        <div style={{paddingTop:'30px'}}>
+          <div style={{ backgroundImage: `url("${process.env.BASE_URL}/Rectangle 40.png")`, backgroundSize: 'contain', height: '100vh' }}>
+            <h1 style={{textAlign:'center',paddingBottom:'20px'}}>فريقنا</h1>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
+              <div style={{width:'40%', marginRight:'20px',display:'flex',paddingLeft:'20%',paddingTop:'0', paddingBottom:'0'}}>
+                <Image src={`${process.env.BASE_URL}/team_safia.png`} alt='' width={100} height={100} style={{height:'100%', width:'100%'}} />
+                <div>
+                  <h1 style={{margin:'20px 0',fontSize:'70px',marginLeft:'-80px'}}>صفية أبوقصيصة</h1>
+                </div>
               </div>
-            </div>
-            <div style={{ width: '50%',paddingRight:'20%' }}>
-            <h2 style={{padding:'0 0 20px 0',color:'#c22026', borderBottom:'solid 2px #c22026'}}>المؤسسة/المستشارة القانونية</h2>
-              <p>
+              <div style={{ width: '50%',paddingRight:'20%' }}>
+                <h2 style={{padding:'0 0 20px 0',color:'#c22026', borderBottom:'solid 2px #c22026'}}>المؤسسة/المستشارة القانونية</h2>
+                <p>
               .صفية أبو قصيصة، محامية مؤهلة، حاصلة على بكلاريوس القانون مرتبة الشرف من جامعة الخرطوم، عضوة في نقابة المحامين السودانية (حيث يتبنى النظام القانوني نظاماً مختلطاً من الشريعة والقانون الانجلوسكسوني)، مرخص لها بتقديم خدمات الاستشارات القانونية بدولة الامارات العربية المتحدة، كما انها مرخص لها بالتقاضي امام كافة المحاكم السودانية بمختلف درجاتها. اكتسبت صفية خبرة قانونية اقليمية عميقة  منذ العام 2015 وذلك لحصولها على فرص تدريب مكثفة لدى مكاتب دولية ومحلية.</p>
-              <p style={{marginBottom:'0'}}>
+                <p style={{marginBottom:'0'}}>
               صفية تأتي كمستشارة قانونية دقيقة ومبادرة . بالإضافة إلى كونها متمكنة في مختلف القوانين، تتمتع صفية بمجموعة من المهارات المهنية والصفات الشخصية التي تعتبر اساسية لتقديم خدمات متكاملة لتحقيق تغطية قانونية مثلى لعملائنا. إنها متميزة في #قانون_الشركات #القانون_التجاري #قانون_الاستثمار، مع تركيز خاص على #قوانين الاتصالات الناتجة عن خبرتها في مجال #تقنية المعلومات والاتصالات.              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div style={{paddingTop:'30px',direction:'rtl'}}>
-        <div style={{ backgroundImage: `url("${process.env.BASE_URL}/Rectangle 40.png")`, backgroundSize: 'contain', height: '100vh' }}>
-          <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
-          <div  style={{width:'40%', marginLeft:'20px',display:'flex',paddingRight:'20%',paddingTop:'0', paddingBottom:'0'}}>
-              <Image src={`${process.env.BASE_URL}/issam.png`} alt='' width={100} height={100} style={{height:'100%', width:'100%',transform:'scaleX(-1)'}} />
-            </div>
-            <div style={{ width: '50%',paddingLeft:'20%' }}>
-              <div style={{marginRight:'-100px', width:'80%'}}>
-              <h1 style={{margin:'20px 0',fontSize:'70px'}}>عصام أبوقصيصة</h1>
               </div>
-            <h2 style={{padding:'0 0 20px 0',color:'#c22026', borderBottom:'solid 2px #c22026'}}> كبير المستشارين</h2>
-              <p>
-              عصام أبو قصيصة، محام ذو خبرة متنوعة منذ عام 1975، حاصل على درجة البكالوريوس في القانون من جامعة القاهرة - فرع السودان، ودراسات عليا من جامعة بريستول - المملكة المتحدة.</p>
-              <p>
-              عصام أبوقصيصة ممارس في مختلف مجالات القانون #القانون_التجاري #قوانين_المالية #قانون النقل البحري #التجارة الدولية #التكنولوجيا المالية #الملكية_الفكرية، بخبرة واسعة في #البنوك_الإسلامية وقوانين الأموال الإسلامية بشكل عام. </p>              
-              <p style={{marginBottom:'0'}}>
-              عمل كمحامٍ فدرالي مرخص، ويمارس الآن كاستشاري قانوني في السودان والإمارات، عمل كمستشار قانوني وشرعي لعدة بنوك إسلامية رائدة وشركات تجارية في الإمارات ومنطقة الشرق الأوسط وشمال أفريقيا، واعترف به من قبل مصرف الإمارات المركزي كمستشار شرعي ومراقب شرعي وتم تسجيله كخبير في قانون البنوك الإسلامية لدى محاكم دبي.</p>            </div>
+            </div>
           </div>
         </div>
-      </div>
+
+        <div style={{paddingTop:'30px',direction:'rtl'}}>
+          <div style={{ backgroundImage: `url("${process.env.BASE_URL}/Rectangle 40.png")`, backgroundSize: 'contain', height: '100vh' }}>
+            <div style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'flex-end'}}>
+              <div style={{width:'40%', marginLeft:'20px',display:'flex',paddingRight:'20%',paddingTop:'0', paddingBottom:'0'}}>
+                <Image src={`${process.env.BASE_URL}/issam.png`} alt='' width={100} height={100} style={{height:'100%', width:'100%',transform:'scaleX(-1)'}} />
+              </div>
+              <div style={{ width: '50%',paddingLeft:'20%' }}>
+                <div style={{marginRight:'-100px', width:'80%'}}>
+                  <h1 style={{margin:'20px 0',fontSize:'70px'}}>عصام أبوقصيصة</h1>
+                </div>
+                <h2 style={{padding:'0 0 20px 0',color:'#c22026', borderBottom:'solid 2px #c22026'}}> كبير المستشارين</h2>
+                <p>
+              عصام أبو قصيصة، محام ذو خبرة متنوعة منذ عام 1975، حاصل على درجة البكالوريوس في القانون من جامعة القاهرة - فرع السودان، ودراسات عليا من جامعة بريستول - المملكة المتحدة.</p>
+                <p>
+              عصام أبوقصيصة ممارس في مختلف مجالات القانون #القانون_التجاري #قوانين_المالية #قانون النقل البحري #التجارة الدولية #التكنولوجيا المالية #الملكية_الفكرية، بخبرة واسعة في #البنوك_الإسلامية وقوانين الأموال الإسلامية بشكل عام. </p>              
+                <p style={{marginBottom:'0'}}>
+              عمل كمحامٍ فدرالي مرخص، ويمارس الآن كاستشاري قانوني في السودان والإمارات، عمل كمستشار قانوني وشرعي لعدة بنوك إسلامية رائدة وشركات تجارية في الإمارات ومنطقة الشرق الأوسط وشمال أفريقيا، واعترف به من قبل مصرف الإمارات المركزي كمستشار شرعي ومراقب شرعي وتم تسجيله كخبير في قانون البنوك الإسلامية لدى محاكم دبي.</p>            </div>
+            </div>
+          </div>
+        </div>
       </a>
       <a name="news">
-      <div className="content news">
-        <div className="news_scroll">
-          <div className="news_block" dir="rtl">
-            <p style={{textAlign:'right', marginRight:'40px'}}>يسرنا أن نعلن عن شراكتنا الاستراتيجية مع مكتب حمدان رشود الزيودي للمحاماة، الذي يمثله السيد حمدان راشد علي رشود الزيودي، )قاضي استئناف سابق ومحام مرخص له بالتقاضي امام كافة المحاكم بدولة الامارات العربية المتحدة) وهي شراكة تعد بتعزيز خدماتنا وتوفير حلول قانونية شاملة. من خلال دمج قوتنا وخبرتنا، نهدف إلى تقديم نهج شامل لحماية جهودنا التجارية. تأكيدهم على التدابير الوقائية وخبرتهم في حل النزاعات، جنبًا إلى جنب مع التزامنا بالتميز وتكريس جهودنا وخلاصة معرفتنا لتحقيق مصلحة العمي ، سيضمن التمثيل الخبير والإرشاد الاستراتيجي لتحقيق النتائج المثلى. سنستغل معًا تضامننا لتقديم خدمة شاملة وسلسة تلبي الاحتياجات المتنوعة لعملائنا.</p>
+        <div className="content news">
+          <div className="news_scroll">
+            <div className="news_block" dir="rtl">
+              <p style={{textAlign:'right', marginRight:'40px'}}>يسرنا أن نعلن عن شراكتنا الاستراتيجية مع مكتب حمدان رشود الزيودي للمحاماة، الذي يمثله السيد حمدان راشد علي رشود الزيودي، )قاضي استئناف سابق ومحام مرخص له بالتقاضي امام كافة المحاكم بدولة الامارات العربية المتحدة) وهي شراكة تعد بتعزيز خدماتنا وتوفير حلول قانونية شاملة. من خلال دمج قوتنا وخبرتنا، نهدف إلى تقديم نهج شامل لحماية جهودنا التجارية. تأكيدهم على التدابير الوقائية وخبرتهم في حل النزاعات، جنبًا إلى جنب مع التزامنا بالتميز وتكريس جهودنا وخلاصة معرفتنا لتحقيق مصلحة العمي ، سيضمن التمثيل الخبير والإرشاد الاستراتيجي لتحقيق النتائج المثلى. سنستغل معًا تضامننا لتقديم خدمة شاملة وسلسة تلبي الاحتياجات المتنوعة لعملائنا.</p>
+            </div>
           </div>
+          <h1>آخر الأخبار</h1>
         </div>
-        <h1>آخر الأخبار</h1>
-      </div>
       </a>
     </>
   );
