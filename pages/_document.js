5f3856f990a45e451ef14b03bff7ebf938e2e8c2
BalqeesHamdi <Balqees.icode@gmail.com>
2024-07-24 23:10:16 +0300

    og image

--- pages/_document.js
+++ pages/_document.js
@@ -10,7 +10,7 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" type="image/svg+xml" href="/logo_sia.png" />
+          <link rel="icon" type="image/svg+xml" href="/og_logo.jpg" />
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css"
             integrity="sha512-1ycn6IcaQQ40/MKBW2W4Rhis/DbILU74C1vSrLJxCq57o941Ym01SwNsOMqvEBFlcgUa6xLiPY/NS5R+E6ztJQ=="
             crossOrigin="anonymous" referrerPolicy="no-referrer" />
@@ -18,7 +18,7 @@ class MyDocument extends Document {
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
           <link href="https://fonts.googleapis.com/css2?family=Almarai:wght@300;400;700;800&display=swap" rel="stylesheet" />
           <meta property="description" content="We understand the importance of staying ahead in today's interconnected global business landscape. Our commitment to leveraging technology and innovation allows us to provide tailored legal solutions that meet the evolving needs of our clients." />
-          <meta property="og:image" content="/logo_sia.png"></meta>
+          <meta property="og:image" content="/og_logo.jpg"></meta>
           <meta property="og:site_name" content="Safia Abugisseisa Law Firm"></meta>
           <meta property="og:title" content="Safia Abugisseisa Law Firm"></meta>
           <meta property="og:url" content="www.safiaabugisseisa.com"></meta>
